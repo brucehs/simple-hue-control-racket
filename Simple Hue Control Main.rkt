151f543d457ec8985ac86bd271ce241d74717a13
Bruce Steinberg <git@brucesteinberg.co>
2015-08-04 05:04:23 -0400

    Moved cueList% and cue% to their own file
    Moved custom classes to shc-classes.rkt.

--- Simple Hue Control Main.rkt
+++ Simple Hue Control Main.rkt
@@ -2,7 +2,8 @@
 
 (require net/http-client
          net/uri-codec
-         json)
+         json
+         "shc-classes.rkt")
 
 (compile-allow-set!-undefined #t)
 
@@ -69,58 +70,7 @@
 ; available via an API call. It may be better to ask the user to specify
 ; a time upon saving a cue.
 
-(define cueList%
-  (class object%
-    (super-new)
-    (init-field [label ""])
-    (init-field [children '()])
-    (define/public (get-label) label)
-    (define/public (get-children) children)
-    (define/public (set-children listOfChildren)
-      (set-field! children this listOfChildren))))
 
-(define cue%
-  (class object%
-    (super-new)
-    (define stateJson     (hash
-                           'on #t
-                           'bri 0
-                           'hue 0
-                           'sat 0
-                           'xy (list 0 0)
-                           'ct 0
-                           'alert "none"
-                           'effect "none"
-                           'colormode "hs"
-                           'reachable #t))
-    (init-field [label ""])
-    (init-field [jsonResponse (hash 'light stateJson
-                                    'type "Extended color light"
-                                    'name "Generic Name"
-                                    'modelid "LCT001"
-                                    'swversion "")])
-    (init-field [parent '(object:cueList%)])
-    ; Time is in milliseconds. Does not need to be adjusted when sent to bridge.
-    (init-field [time 4])
-    (define/public (get-label) label)
-    (define/public (get-parent) parent)
-    (define/public (get-json) jsonResponse)
-    (define/public (get-time) time)
-    (define/public (set-label newLabel)
-      (set-field! label this newLabel))
-    (define/public (set-json newJson)
-      (set-field! jsonResponse this newJson))
-    (define/public (set-parent parentCueList)
-      (set-field! children parentCueList 
-                  (append (get-field children parentCueList) (list this)))
-      (set-field! parent this parentCueList))
-    (define/public (set-time newTime)
-      (cond
-        ((equal? (string->number newTime) #f)
-         (set-field! time this 0))
-        (else
-         (set-field! time this
-                     (inexact->exact (* (string->number newTime) 10))))))))
 
 ; Hack for Workshop: Create a main Cue List.
 
