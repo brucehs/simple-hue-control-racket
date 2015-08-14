f0ac498d2fd29f7a5d1f2fed3276a867ba91a620
Bruce Steinberg <git@brucesteinberg.co>
2015-08-14 16:22:42 -0400

    Created Lamp Menu and Patch Window
    Create the GUI for setting the patch for bulbs->channels. The underlying logic is not there yet.

--- Simple Hue Control Main.rkt
+++ Simple Hue Control Main.rkt
@@ -719,7 +719,64 @@
 
 (define hueWindowMenuBar (new menu-bar% [parent hueWindow]))
 
-; Bridge Menu
+;; Lamp Window
+
+(define hue-window-menu-lamp (new menu% [parent hueWindowMenuBar]
+                                  [label "Lamp"]))
+(define hue-window-menu-lamp-patch (new menu-item% [parent hue-window-menu-lamp]
+                                        [label "Patch"]
+                                        [callback (lambda (menu event)
+                                                    (send lamp-patch-dialog show #t))]))
+
+;; Patch Dialog
+
+(define lamp-patch-dialog (new dialog% [label "Patch Lamps"]
+                               [min-width 300]
+                               [min-height 800]))
+
+(define assigned-light-panel (new vertical-panel% [parent lamp-patch-dialog]
+                                  [alignment '(left top)]
+                                  [horiz-margin 10]
+                                  [vert-margin 15]
+                                  [min-width 50]
+                                  [min-height 600]))
+
+(for ([i (in-range 1 range-of-lights)])
+  (new text-field%
+       [parent assigned-light-panel]
+       [label (cond
+                ((< i 10)
+                (string-append "  Channel " (number->string i) "         ""Bulb:"))
+                (else
+                 (string-append "  Channel " (number->string i) "        ""Bulb:")))]
+       [init-value (number->string
+                    (send
+                     (list-ref (send mainPatch get-children) (- i 1))
+                     get-bulb))]
+       [min-width 40]
+       [stretchable-width 40]
+       [vert-margin 5]))
+
+(define patch-button-panel (new horizontal-panel% [parent lamp-patch-dialog]
+                                [alignment '(center bottom)]
+                                [horiz-margin 10]
+                                [vert-margin 15]))
+
+(define patch-cancel-button (new button% [parent patch-button-panel]
+                                 [label "Cancel"]
+                                 [callback (lambda (button event)
+                                             (send lamp-patch-dialog show #f))]
+                                 [horiz-margin 15]))
+
+;; Needs procedure to actually patch the lights.
+
+(define patch-set-button (new button% [parent patch-button-panel]
+                              [label "Set"]
+                              [callback (lambda (button event)
+                                          (send lamp-patch-dialog show #f))]
+                              [horiz-margin 15]))
+
+;; Bridge Menu
 (define hueWindowMenuBridge (new menu% [parent hueWindowMenuBar]
                                  [label "Bridge"]))
 (define hueWindowMenuBridgeBridgeAddress (new menu-item% [parent hueWindowMenuBridge]
@@ -736,7 +793,7 @@
                                                [callback (lambda (menu event)
                                                            (send updateFirmwareDialog show #t))]))
 
-; Set Bridge Address Dialog
+;; Set Bridge Address Dialog
 (define bridgeAddressDialog (new dialog% [label "Enter Bridge Address"]
                                  [min-width 300]
                                  [min-height 100]))
