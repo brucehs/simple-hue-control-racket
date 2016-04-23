17d8661b2859f9fa8f9e6b958506f1eac9aa3f15
Bruce Steinberg <git@brucesteinberg.co>
2016-04-22 23:07:59 -0400

    Commented Out Patch Dialog
    Will now be creating the Patch Dialog from shc-gui.rkt.

--- Simple Hue Control Main.rkt
+++ Simple Hue Control Main.rkt
@@ -833,57 +833,57 @@
 
 ;; Patch Dialog
 
-(define lamp-patch-dialog (new dialog% [label "Patch Lamps"]
-                               [min-width 300]
-                               [min-height 600]))
-
-(define assigned-light-panel (new vertical-panel% [parent lamp-patch-dialog]
-                                  [alignment '(left top)]
-                                  [horiz-margin 10]
-                                  [vert-margin 15]
-                                  [min-width 50]
-                                  [min-height 600]))
-
-(for ([i (in-range 1 range-of-lights)])
-  (new text-field%
-       [parent assigned-light-panel]
-       [label (cond
-                ((< i 10)
-                 (string-append "  Channel " (number->string i) "         ""Bulb:"))
-                (else
-                 (string-append "  Channel " (number->string i) "        ""Bulb:")))]
-       [init-value (number->string
-                    (send
-                     (list-ref (send mainPatch get-children) (- i 1))
-                     get-bulb))]
-       [min-width 40]
-       [stretchable-width 40]
-       [vert-margin 5]))
-
-(define patch-button-panel (new horizontal-panel% [parent lamp-patch-dialog]
-                                [alignment '(center bottom)]
-                                [horiz-margin 10]
-                                [vert-margin 15]))
-
-(define patch-cancel-button (new button% [parent patch-button-panel]
-                                 [label "Cancel"]
-                                 [callback (lambda (button event)
-                                             (send lamp-patch-dialog show #f))]
-                                 [horiz-margin 15]))
-
-(define patch-set-button (new button% [parent patch-button-panel]
-                              [label "Set"]
-                              [callback (lambda (button event)
-                                          (set-patch!
-                                           mainPatch
-                                           assigned-light-panel)
-                                          (save-show
-                                           mainPatch
-                                           mainList
-                                           saved-show-write-port)
-                                          (send lamp-patch-dialog show #f))]
-                              [style '(border)]
-                              [horiz-margin 15]))
+;(define lamp-patch-dialog (new dialog% [label "Patch Lamps"]
+;                               [min-width 300]
+;                               [min-height 600]))
+;
+;(define assigned-light-panel (new vertical-panel% [parent lamp-patch-dialog]
+;                                  [alignment '(left top)]
+;                                  [horiz-margin 10]
+;                                  [vert-margin 15]
+;                                  [min-width 50]
+;                                  [min-height 600]))
+;
+;(for ([i (in-range 1 range-of-lights)])
+;  (new text-field%
+;       [parent assigned-light-panel]
+;       [label (cond
+;                ((< i 10)
+;                 (string-append "  Channel " (number->string i) "         ""Bulb:"))
+;                (else
+;                 (string-append "  Channel " (number->string i) "        ""Bulb:")))]
+;       [init-value (number->string
+;                    (send
+;                     (list-ref (send mainPatch get-children) (- i 1))
+;                     get-bulb))]
+;       [min-width 40]
+;       [stretchable-width 40]
+;       [vert-margin 5]))
+;
+;(define patch-button-panel (new horizontal-panel% [parent lamp-patch-dialog]
+;                                [alignment '(center bottom)]
+;                                [horiz-margin 10]
+;                                [vert-margin 15]))
+;
+;(define patch-cancel-button (new button% [parent patch-button-panel]
+;                                 [label "Cancel"]
+;                                 [callback (lambda (button event)
+;                                             (send lamp-patch-dialog show #f))]
+;                                 [horiz-margin 15]))
+;
+;(define patch-set-button (new button% [parent patch-button-panel]
+;                              [label "Set"]
+;                              [callback (lambda (button event)
+;                                          (set-patch!
+;                                           mainPatch
+;                                           assigned-light-panel)
+;                                          (save-show
+;                                           mainPatch
+;                                           mainList
+;                                           saved-show-write-port)
+;                                          (send lamp-patch-dialog show #f))]
+;                              [style '(border)]
+;                              [horiz-margin 15]))
 
 ;; Bridge Menu
 (define hueWindowMenuBridge (new menu% [parent control-window-menu-bar]
